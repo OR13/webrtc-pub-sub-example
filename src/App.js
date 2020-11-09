84679ee57e7756a3f184ea06847c7c6ca9cc0130
Orie Steele <orie@transmute.industries>
2020-11-08 22:51:12 -0600

    save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react'
+import { doWork } from './doWork'
+import querystring from 'querystring'
+
+const PeerId = require('peer-id')
 function App() {
+  const [state, setState] = React.useState({})
+  React.useEffect(()=>{
+(async ()=>{
+
+  const {libp2p } = await doWork();
+  const peerId = libp2p.peerId.toB58String();
+
+  // // Listen for new peers
+  // libp2p.on('peer:discovery', (peerId) => {
+  //   console.log(`Found peer ${peerId.toB58String()}`)
+  // })
+
+  // // Listen for new connections to peers
+  // libp2p.connectionManager.on('peer:connect', (connection) => {
+  //   console.log(`Connected to ${connection.remotePeer.toB58String()}`)
+  // })
+
+  // // Listen for peers disconnecting
+  // libp2p.connectionManager.on('peer:disconnect', (connection) => {
+  //   console.log(`Disconnected from ${connection.remotePeer.toB58String()}`)
+  // })
+
+ 
+  // const multiaddrs = libp2p.multiaddrs;
+ let peers = [];
+  if (window.location.search.split("?").pop()){
+    ({peers} = querystring.parse(window.location.search.split("?").pop()))
+    peers = JSON.parse(peers)
+   
+  }
+  const topic = 'magical-topic'
+  libp2p.pubsub.on(topic, (msg) => {
+    console.log(`${peerId} received: `, msg)
+  })
+
+  await libp2p.pubsub.subscribe(topic)
+
+  if (peers.length){
+    peers.forEach(async (peerId)=>{
+      peerId = PeerId.createFromB58String(peerId)
+      libp2p.peerStore.addressBook.set( peerId , libp2p.multiaddrs)
+      await libp2p.dial(peerId)
+    })
+  } else {
+    setInterval(() => {
+      libp2p.pubsub.publish(topic, new Uint8Array(Buffer.from('Bird bird bird, bird is the word!')));
+    }, 1000)
+  }
+
+  setState((state)=> {
+    return {...state, peerId}
+  })
+})()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 }
